fa4e07937be2268a3cfdacfb7fd42927b498d6e7
sharon-launchcode <sharon.launchcode@gmail.com>
2018-03-16 23:27:33 -0400

    Added Categories by creating a CategoryController in Controllers, added private readonly CheeseDbContext context;  and then public CategoryController(CheeseDbContext dbContext)  {context = dbContext;} which created a private field of type CheeseDbContext

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
