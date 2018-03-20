67921405abaac4fedf877f031ba192e2ba4fc69c
sharon-launchcode <sharon.launchcode@gmail.com>
2018-03-20 15:28:21 -0400

    update table in Views\Cheese\Index and check

--- src/CheeseMVC/Migrations/20180320192056_Part2.Designer.cs
+++ src/CheeseMVC/Migrations/20180320192056_Part2.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180320192056_Part2")]
+    partial class Part2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
