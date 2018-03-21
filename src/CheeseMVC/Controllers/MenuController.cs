7e4782265fa8469de256cbe06ff560a2c0910da1
sharon-launchcode <sharon.launchcode@gmail.com>
2018-03-21 16:06:14 -0400

    Sharon you idiot it is CheeseDbContext NOT MenuDbContext

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
